9723d44fc4c11128bc8e20bf887a4de768d40154
menghuanwd <651019063@qq.com>
2022-02-01 20:27:28 +0800

    first web3

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+
+const Navbar = () => {
+    return (
+        <>
+            <nav className="w-full flex md:justify-center justify-center">
+                <div className="md:flex-[0.5] flex-initial">
+
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar;
