f370a80d7cefc83060911f9bc8decc333e531d2d
menghuanwd <651019063@qq.com>
2022-02-04 00:07:24 +0800

    first web3

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,27 @@
+import logo from "../../images/logo.png"
+
+const NavbarItem = ({title, ss}) => {
+    return (
+        <li className={ss}>
+            {title}
+        </li>
+    )
+}
 
 const Navbar = () => {
     return (
         <>
-            <nav className="w-full flex md:justify-center justify-center">
-                <div className="md:flex-[0.5] flex-initial">
-
+            <nav className="w-full flex md:justify-center justify-center items-center p-4">
+                <div className="md:flex[0-5] flex-initial justify-center items-center">
+                    <img src={logo} className="w-32 cursor-pointer"/>
                 </div>
+                <ul>
+                    {
+                        ['Market', 'Exchange', 'Tutorials'].map((item, index) => (
+                            <NavbarItem key={item + index} title={item} ss={index}/>
+                        ))
+                    }
+                </ul>
             </nav>
         </>
     )
--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -1,6 +1,6 @@
 const Transactions = () => {
     return (
-        <h1>Transactions</h1>
+        <h1>Transaction3s</h1>
     )
 }
 
